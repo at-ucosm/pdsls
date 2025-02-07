5d1e2aa3e518ba1bfbc80905bc7c83c86549476a
phil <uniphil@gmail.com>
2025-02-07 12:32:31 -0500

    hash router, no protocol handler

--- docs/assets/yaml-CqaELC0g.js
+++ docs/assets/yaml-CqaELC0g.js
@@ -1,4 +1,4 @@
-import{m as l}from"./index-Duivw62Q.js";/*!-----------------------------------------------------------------------------
+import{m as l}from"./index-C6AMYFb8.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
